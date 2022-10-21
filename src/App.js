f74ee7bd85ebb2f2c71992e158c3b559b446d486
“Green <greenojegwo@gmail.com>
2022-10-21 13:03:50 +0100

    catfacts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [catFact, setCatFact] = useState("")
 
+  const CatThings = () => {
+    axios
+      .get("https://catfact.ninja/fact")
+      .then((res) => {
+        setCatFact(res.data.fact);
+      });
+  };
+  useEffect(() => {
+    CatThings()
+  }, [])
+
+  return (
+    <div className="cat">
+
+      <h3>Cat Facts You Should Know 😻</h3>
+      <hr></hr>
+
+      <div className='main_fact'>
+        <a href="https://www.freeiconspng.com/img/40358" title="Image from freeiconspng.com"><img src="https://www.freeiconspng.com/uploads/baby-cat-png-12.png" width="350" alt="baby cat png" /></a>
+
+        <div className='main_fact_two'>
+          <button onClick={CatThings}>Generate Cat Facts</button>
+          <marquee behavior="scroll-through" direction="left-to-right">FACT</marquee>
+          <h5 className='fact_details'>
+            {/* <span>FACT</span> <br /> */}
+            {catFact}
+          </h5>
+        </div>
+      </div>
+
+      <p>Created By <a href="https://greenojegwo.netlify.app/">Green</a></p>
+    </div>
+  )
+}
 export default App;
